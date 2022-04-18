452bdf520a69bad12454801897bce4d35ebab9c4
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-04-18 16:17:34 -0600

    got info from the user and displayed it on the terminal

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your favorite color");
+string color = Console.ReadLine();
+Console.WriteLine($"Your favorite color is {color}");
