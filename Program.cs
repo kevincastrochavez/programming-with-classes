12ab60e1306492f4f87d107890007d5e5c8588d2
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-04-18 18:02:28 -0600

    learned about typed code, reading function and string interpolation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Types must be specified
+int x = 5;
+Console.WriteLine(x);
+
+// Read from Terminal
 Console.WriteLine("What is your favorite color");
 string color = Console.ReadLine();
 Console.WriteLine($"Your favorite color is {color}");
+
+// String Interpolation
+string school = "BYU-Idaho";
+Console.WriteLine($"I'm studying at {school}.");
