8cf919bf3efdde771383b55ac1d588b54265481c
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-04-20 14:51:35 -0600

    finished solo prep #2

---
+++ soloprep_two/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Type your grade percentage: ");
+string gradePercentage = Console.ReadLine();
+
+int percentage = int.Parse(gradePercentage);
+
+string letter = "";
+
+if (percentage >= 90) {
+    letter = "A";
+} else if (percentage >= 80) {
+    letter = "B";
+} else if (percentage >= 70) {
+    letter = "C";
+} else if (percentage >= 60) {
+    letter = "D";
+} else {
+    letter = "F";
+}
+
+Console.WriteLine($"You got a {letter}");
+
+if (percentage >= 70) {
+    Console.WriteLine("Congratulations! You passed the class");
+} else {
+    Console.WriteLine("Try again later");
+}
