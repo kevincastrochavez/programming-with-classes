b0bbb0e12e0f5aca554e947290f9f89e3ed915e8
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-04-20 14:24:21 -0600

    finished checkpoint from week 1

---
+++ checkpoint_weekOne/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("What is your first name: ");
+string firstName = Console.ReadLine();
+
+Console.Write("What is your last name: ");
+string lastName = Console.ReadLine();
+
+Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
