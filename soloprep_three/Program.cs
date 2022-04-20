876e32b23846f98edb4b51202c6be46be4b94283
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-04-20 17:13:08 -0600

    finished solo prep #3, project generating random numbers

---
+++ soloprep_three/Program.cs
@@ -0,0 +1,19 @@
+﻿Random randomGenerator = new Random();
+int magicNumber = randomGenerator.Next(1, 101);
+Console.WriteLine(magicNumber);
+
+int guess = -1;
+
+while (guess != magicNumber) {
+    Console.Write("What is your guess?: ");
+    guess = int.Parse(Console.ReadLine());
+    
+    if (magicNumber > guess) {
+        Console.WriteLine("Go higher");
+    } else if (magicNumber < guess) {
+        Console.WriteLine("Go lower");
+    } else {
+        Console.WriteLine("You guessed it!");
+    }
+}
+
