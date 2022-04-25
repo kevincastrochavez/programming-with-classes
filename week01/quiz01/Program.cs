094d7eb749558a6d638a84ba6639b812e946ab04
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-04-25 14:19:17 -0600

    completed quiz in class

---
+++ week01/quiz01/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter first number: ");
+int firstNumber = int.Parse(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+Console.Write("Enter third number: ");
+int thirdNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{firstNumber}, {secondNumber}, and {thirdNumber}. ({firstNumber} + {secondNumber}) * {thirdNumber} is {(firstNumber + secondNumber) * thirdNumber} and {firstNumber} * {secondNumber} + {secondNumber} * {thirdNumber} is {firstNumber * secondNumber + secondNumber * thirdNumber}");
