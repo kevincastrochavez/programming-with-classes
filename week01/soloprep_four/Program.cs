3d8db06c027816b99b8d32f15b691c42a562a38a
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-04-25 14:27:17 -0600

    finished core requirements for soloprep4

--- week01/soloprep_four/Program.cs
+++ week01/soloprep_four/Program.cs
@@ -1,18 +1,48 @@
 ﻿// Declaring lists in C# (you declare the type of the elements' list)
 
+// List<int> numbers = new List<int>();
+// List<string> words = new List<string>();
+
+// words.Add("phone");
+// words.Add("keyboard");
+// words.Add("mouse");
+
+// Console.WriteLine(words.Count);
+
+// foreach (string word in words) {
+//     Console.WriteLine(word);
+// }
+
+// for (int i = 0; i < words.Count; i++) {
+//     Console.WriteLine(words[i]);
+// }
+
+Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
 List<int> numbers = new List<int>();
-List<string> words = new List<string>();
 
-words.Add("phone");
-words.Add("keyboard");
-words.Add("mouse");
+int number = 1;
 
-Console.WriteLine(words.Count);
+while (number != 0) {
+    Console.Write("Enter number: ");
+    int numberEntered = int.Parse(Console.ReadLine());
+    if (numberEntered != 0) {
+        numbers.Add(numberEntered);
+    }
 
-foreach (string word in words) {
-    Console.WriteLine(word);
+    number = numberEntered;
 }
 
-for (int i = 0; i < words.Count; i++) {
-    Console.WriteLine(words[i]);
+int sum = 0;
+int listCount = numbers.Count;
+int largestNumber = numbers.Max();
+
+foreach (int item in numbers) {
+    sum += item;
 }
+
+int average = sum / listCount;
+
+Console.WriteLine($"The sum is {sum}");
+Console.WriteLine($"The average is {average}");
+Console.WriteLine($"The largest number is {largestNumber}");
