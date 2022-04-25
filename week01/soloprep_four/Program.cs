16a9c38e8e2f6c4747fd5f1a654a02a506105ea9
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-04-25 13:26:39 -0600

    learned about how to declare lists and loop through them

---
+++ week01/soloprep_four/Program.cs
@@ -0,0 +1,18 @@
+﻿// Declaring lists in C# (you declare the type of the elements' list)
+
+List<int> numbers = new List<int>();
+List<string> words = new List<string>();
+
+words.Add("phone");
+words.Add("keyboard");
+words.Add("mouse");
+
+Console.WriteLine(words.Count);
+
+foreach (string word in words) {
+    Console.WriteLine(word);
+}
+
+for (int i = 0; i < words.Count; i++) {
+    Console.WriteLine(words[i]);
+}
