4d430dff1aa234238f13cb8a87abd0c2a773f2cd
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-04-30 11:56:55 -0600

    created function to check for winner

--- week01/tic_tac_toe/Program.cs
+++ week01/tic_tac_toe/Program.cs
@@ -57,4 +57,44 @@
             }
             while (flag != 1 && flag != -1);
         }
+
+        private static int CheckWin()
+        {
+            if (arr[1] == arr[2] && arr[2] == arr[3])
+            {
+                return 1;
+            }
+            else if (arr[4] == arr[5] && arr[5] == arr[6])
+            {
+                return 1;
+            }
+            else if (arr[6] == arr[7] && arr[7] == arr[8])
+            {
+                return 1;
+            }
+            else if (arr[1] == arr[4] && arr[4] == arr[7])
+            {
+                return 1;
+            }
+            else if (arr[2] == arr[5] && arr[5] == arr[8])
+            {
+                return 1;
+            }
+            else if (arr[3] == arr[6] && arr[6] == arr[9])
+            {
+                return 1;
+            }
+            else if (arr[1] == arr[5] && arr[5] == arr[9])
+            {
+                return 1;
+            }
+            else if (arr[3] == arr[5] && arr[5] == arr[7])
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
         }
+    }
