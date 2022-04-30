3074ff9170bb399dbab38d1f6f01a9ae8c2e08ff
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-04-30 11:59:52 -0600

    added condition in case of a draw and called function to check winner

--- week01/tic_tac_toe/Program.cs
+++ week01/tic_tac_toe/Program.cs
@@ -54,6 +54,9 @@
                     Console.WriteLine("Position {0} is already marked with {1}", playerDecision, arr[playerDecision]);
                     Console.WriteLine("");
                 }
+
+                flag = CheckWin();
+                Console.WriteLine($"Flag: {flag}");
             }
             while (flag != 1 && flag != -1);
         }
@@ -91,6 +94,10 @@
             else if (arr[3] == arr[5] && arr[5] == arr[7])
             {
                 return 1;
+            }
+            else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
+            {
+                return -1;
             }
             else
             {
