2db87057fd42405268dd6ca294a814ac2cecaf4a
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-04-30 11:37:47 -0600

    re-wrote tic-tac-toe function to display the board

---
+++ week01/tic_tac_toe/Program.cs
@@ -0,0 +1,22 @@
+﻿    class Program
+    {
+        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+        static void Main(string[] args)
+        {
+                Board();
+        }
+        
+        private static void Board()
+        {
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+            Console.WriteLine("     |     |      ");
+        }
+    }
